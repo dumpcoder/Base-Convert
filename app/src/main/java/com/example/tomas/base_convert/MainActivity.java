bb07143b27cafd7f6335e67ac7a9ee41007f75b4
Tomas Torres <tomas3torres@live.com>
2018-05-08 00:07:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tomas/base_convert/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tomas.base_convert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+    }
+}
