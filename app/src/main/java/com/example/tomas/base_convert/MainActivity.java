0f5e9b254304d0d9a86c151bda0a9b83c3efbd8f
Tomas Torres <tomas3torres@live.com>
2018-05-09 18:06:34 -0500

    added event handlers.

--- app/src/main/java/com/example/tomas/base_convert/MainActivity.java
+++ app/src/main/java/com/example/tomas/base_convert/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.tomas.base_convert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    int base1;
+    int base2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner1 =  findViewById(R.id.spinner1);
-        Spinner spinner2 =  findViewById(R.id.spinner2);
+        final Spinner spinner1 =  findViewById(R.id.spinner1);
+        final Spinner spinner2 =  findViewById(R.id.spinner2);
+        final EditText editText1 = findViewById(R.id.editText1);
+        final EditText editText2 = findViewById(R.id.editText2);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.bases, android.R.layout.simple_spinner_item);
@@ -21,5 +30,76 @@ public class MainActivity extends AppCompatActivity {
 
         spinner1.setAdapter(adapter) ;
         spinner2.setAdapter(adapter) ;
+
+        spinner1.setSelection(adapter.getPosition("10")); //sets to second item of the array
+        spinner2.setSelection(adapter.getPosition("2")); //set to first item of the array
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                base1 = Integer.parseInt(spinner1.getSelectedItem().toString());
+                editText1.setText(base1 + "");
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                base2 = Integer.parseInt(spinner2.getSelectedItem().toString());
+                editText2.setText(base2 + "");
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(getCurrentFocus() == editText1) {
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        editText2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(getCurrentFocus() == editText2) {
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
