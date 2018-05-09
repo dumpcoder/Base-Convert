58f50405d8554e35cc819d12f243713fa2824a6c
Tomas Torres <tomas3torres@live.com>
2018-05-09 16:35:52 -0500

    "drop down list update"

--- app/src/main/java/com/example/tomas/base_convert/MainActivity.java
+++ app/src/main/java/com/example/tomas/base_convert/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tomas.base_convert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner spinner1 =  findViewById(R.id.spinner1);
+        Spinner spinner2 =  findViewById(R.id.spinner2);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.bases, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(adapter) ;
+        spinner2.setAdapter(adapter) ;
     }
 }
